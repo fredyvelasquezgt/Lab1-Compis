2a5c321484ca6933c886fbabbfe145e0ab8b9969
Fredy Velásquez <59968764+fredyvelasquezgt@users.noreply.github.com>
2023-08-02 23:05:01 -0600

    Update yaplErrorListener.py

--- yaplErrorListener.py
+++ yaplErrorListener.py
@@ -1,15 +1,17 @@
-
 from antlr4 import *
 from antlr4.error.ErrorListener import *
 
-class yaplErrorListener(ErrorListener):
+class YaplErrorListener(ErrorListener):
 
     ANSI_RESET = "\u001B[0m"
     ANSI_RED = "\u001B[31m"
 
-    def syntaxError(self, recognizer, offendingSymbol, line, column, msg, e):
+    def display_error(self, line, column, message):
         print("\n" + self.ANSI_RED)
         print("----------------------------- ERROR -----------------------------")
-        print("Error: position " + str(line) + ":" + str(column) + " " + msg)
+        print(f"Error: position {line}:{column} {message}")
         print("-----------------------------------------------------------------")
         print("\n" + self.ANSI_RESET)
+
+    def syntaxError(self, recognizer, offendingSymbol, line, column, msg, e):
+        self.display_error(line, column, msg)
